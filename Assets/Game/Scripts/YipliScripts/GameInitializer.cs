efaf03bc48b0bcbcef0045a7f4d5b329cfa038bd
vismay113 <vismay.patel1103@gmail.com>
2021-05-05 13:28:50 +0530

    Dynamic link problem is solved.
    Latest gamelib is already in the game

--- Assets/Game/Scripts/YipliScripts/GameInitializer.cs
+++ Assets/Game/Scripts/YipliScripts/GameInitializer.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using Firebase.DynamicLinks;
 
 public class GameInitializer : MonoBehaviour
 {
     public YipliConfig currentYipliConfig;
+
     private void Awake()
     {
         currentYipliConfig.gameId = "joyfuljumps";
         currentYipliConfig.gameType = GameType.FITNESS_GAMING;
-    }
+    }  
 }
